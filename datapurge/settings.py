0c6ce859420c620e499fe57b7d0b90b05ac36719
swistakm <swistakm@gmail.com>
2013-02-14 13:34:39 +0100

    change default DATAPURGE_MODELS settings value

--- datapurge/settings.py
+++ datapurge/settings.py
@@ -6,4 +6,4 @@ DATAPURGE_GRACEFULLY = getattr(settings, "DATAPURGE_GRACEFULLY", False)
 DATAPURGE_GRACE_STEP = getattr(settings, "DATAPURGE_GRACE_STEP", 100)
 DATAPURGE_GRACE_WAIT = getattr(settings, "DATAPURGE_GRACE_WAIT", timezone.timedelta(seconds=0.3))
 
-DATAPURGE_MODELS = getattr(settings, "DATAPURGE_MODELS", ())
+DATAPURGE_MODELS = getattr(settings, "DATAPURGE_MODELS", {})
