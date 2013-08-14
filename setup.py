85d96db68b8ebec80876f37341e8127192da58e1
swistakm <swistakm@gmail.com>
2013-08-14 18:25:33 +0200

    fix version in setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 import os
 from setuptools import setup
 
+from datapurge import __version__ as version
+
 README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 
 # allow setup.py to be run from any path
@@ -9,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name = 'django-datapurge',
-    version = '0.1',
+    version = version,
     packages = ['datapurge'],
     include_package_data = True,
     license = 'BSD License',
